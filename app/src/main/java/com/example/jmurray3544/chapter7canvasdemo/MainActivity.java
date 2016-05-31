6e7ddb91fafad899b381a7ac8baa30b90958682c
James Murray <Jmurray3544@conestogac.on.ca>
2016-05-31 09:47:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jmurray3544/chapter7canvasdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.jmurray3544.chapter7canvasdemo;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Get areference ot our ImageView in the layout
+        ImageView ourFrame = (ImageView) findViewById(R.id.imageView);
+
+        //Create a bitmap object to use as our canvas
+        Bitmap ourBitmap = Bitmap.createBitmap(600,750, Bitmap.Config.ARGB_8888);
+        Canvas ourCanvas = new Canvas(ourBitmap);
+
+        //A paint object that does our drawing, on our canvas
+        Paint paint = new Paint();
+
+        //Set the background color
+        ourCanvas.drawColor(Color.DKGRAY);
+
+        //Change the color of the virtual paint brush
+        paint.setColor(Color.argb(255, 255, 255, 255));
+
+        //Now draw a load of stuff on our canvas
+        ourCanvas.drawText("Score: 42 Lives: 3 Hi: 97", 10, 10, paint);
+        ourCanvas.drawLine(10, 50, 375, 50, paint);
+        ourCanvas.drawCircle(110, 160, 100, paint);
+        ourCanvas.drawPoint(10, 260, paint);
+
+        //Now put the canvas in the frame
+        ourFrame.setImageBitmap(ourBitmap);
+
+    }
+
+
+}
